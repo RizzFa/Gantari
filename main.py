18c496c55558694147e25d3919efccdb57fb6c13
Gantari <153635318+RizzFa@users.noreply.github.com>
2024-01-31 02:03:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -54,41 +54,4 @@ time.sleep(1)
 print("1")
 time.sleep(1)
 
-with open('./config.json') as f:
-    config_data = json.load(f)
-    token = config_data['Config'][0]['token']  # config.json
-    message = config_data['Config'][0]['message']  # config.json
-
-header_data = { 
-    "Content-Type": "application/json", 
-    "User-Agent": "DiscordBot", 
-    "Authorization": token  
-} 
-
-def get_connection(): 
-    return http.client.HTTPSConnection("discord.com", 443) 
-
-def send_message(conn, channel_id, message_data): 
-    try: 
-        conn.request("POST", f"/api/v10/channels/{channel_id}/messages", message_data, header_data) 
-        resp = conn.getresponse() 
-         
-        if 199 < resp.status < 300: 
-            print("Pesan sukses terkirim..")
-        else: 
-            print(f"HTTP {resp.status}: {resp.reason}")
-    except: 
-        print("Error.") 
-
-def main(): 
-    message_data = { 
-        "content": message + "\n\n**=============================\nAuto Post by Gantari Community\n=============================**", 
-        "tts": False
-    } 
-
-    send_message(get_connection(), channel_id, json.dumps(message_data)) 
-
-if __name__ == '__main__': 
-    while True:    
-        main()      
-        time.sleep(waktu)
+jf+IMO+q5ajcc3kT2/dcXr7+rySyNFjut1kDcopdu/lvTUB/8bDwaz3wXiWAxEZR2yjxlQaR2A4LbiCYnyHj0aFyk4IXKUvKhJ4vyZjjnSXh8AfUDfRDxZBqv8TnIDR/jzRiCihoixe6T66yFr3iwA8Pv8jx3owrRmGIT7DlZL+8qTyDHmv7yxQ8eMTQgV7FZcnKwML7+eLlCN8qnM/ggkWyqAyVdSzwycDHZocLQlvqzdd0EveNcW8bg6CYq0IDgSeKo4neR3PIPDd+1zHlBW0LmOIbb2qtJ2spQwav25GZjTP5Db6Cz3ziPLabQ0U5HZpJTMNQQ1sqeldscQweSxodtzmZdWe7B25R184ljcg1t9aUjY+60AcyBBF7mUaGerufgd3AidzUcGYSFCBStAmsxwy3qNdEl45bOiY2wOAq7ShG+JPF1zoTk3YpDyK6LauHygWOjiVfsDsUFXiAeKZEZKwYHBeuqrWtb2ByQGUuNJkZql1hZw0EfjjZtjo5bYqao08opSDrKq7zLX6bZ4xzLhfmJKuuAWFHkBeyqAMJ37DVbW3NYQ7CdcmKCwWTZ9oyhgUyCNnWMh4TIPiI3Gq3dKNDL33h3tkW0rIcMEKaVv0WvFDafypCzKF20AwdAON4uvWFSqrkzAMrBsoC9KJ5FYtge+1kpgSRW8jfWuS+2NwnFmwcwn7s8wPblvQiGIJmqjU05qSG7ehXMTcqytgu9lpTL+N3ydB0JT1Evjr59axDPUt4hYlRIXpXh+P0+Qda10XDai3tYVS9PxkWXYltdg5cRRGc5XKZqoHLAUyZHo4LHkiztQit/l0AWVPTqwxa2s6HvAMABHR4iL/WUzVxjMTqUBLhG4ZdkFgoYAmKznUAgeRS4Sst1VM0/kTK+c0zbc6F8lZshkx24tzl+WcJTICHVDOLFzTpuHjCLfqP6zoFskMoW43KNLnd992eDxDa5uvDaiDf/3cgyGfDRTF3zV1roDZAUa3jG+tHPqSYnZTsMu02Y+vFwTLYxAgJmXAf3iju/+1874E0748J5B2v9JC0oZOJpN9fVR9Q1oLNAqfGci+FlOeeN6kVO12S4dLZ9uWS4S1HTH1HXfyiFFJuAI6O++HPlbiWVRSMDNX6W5rEqdd/RQiHLz9M43HY7HqOnuyWwA+mpOd0E/uWmKVzauY5PELaowOtCFCgcppnwEgFyZgt15W/9DRfp07RM8d3hl47IPeVeoEZ7BhXKawE+9VOrejGj4GTMT7CnpMqdIWASx5z2n/At692ZVUGlbd2YinL2xk3geD55zMj7pAVMC8QWe36mP6iAT9YY21cMZELR5p6Ot3nRXyok8G9pWUdI25bdshEzaQVBzeiBOJBn204jchqT9CFSk0R4Rt69kZM3M3FejB2KwtzHIyQBzvPxZhKrzPkAwmJSho5pS5RgWxPJGjt/+YaVSPSb5fY0zrngb1Ou8oruy3u+QjSjvnuNbtCDOVw78if6noftDnImZPH6TDPL3nSnZBaQojkxsEzJT/s1YS5z+1P2bZDDQdixSICiLsU0bdmU+4lUswPKLK9YqbEVssqyu/6ChsVpLTno2VTsUVH9CTcCqHWmam7hn47Z9X8c5PIYG4r2JWseNZthnd9E2GtXb8veQ2JNb4iFouOSri1IYjhYTmEoaMT9OIaYZa5tRMxTIiOzTeptAPQx8NyKsLleQZNnxU07uOfKzi1QEHo6DkA5KEqM9I/fzL9U5wWudjaJFzypZYAxUYEIDTmB3wEPacD0Hluy6zXWJW/Ww10k3s2B48g/Bj21lOoAddvxv/HpyZCM1wp0IfdYDOVWD1JO7Gl94nLwGEz33ybFSk+nsZDpkpW1EKGPuHR3YCF2eyRn7JvTeJBAqcHVvloxUMPPkMa4WjQyVn3X10LY2zlSsxhVMMz8W9xWpdvckrDiy2HhJaE4YKAn+1Blew27bonVlmmX2cSNhrL6yXeeMqH8ZNt/JRKfngu4zxtWU1Bx1+MWgFtvU1BG2wC7spOtBH6Ydrx5K5dtKzPRJhswq4Drp5+KWAw8glKU7RtFu++ZX3c6Mrgsrij3sZq429uDtwg3YGcI+3ExKlznDAYbZC1uCyM9PyL+VcFmWv7W90aJkKybfXevqoBG/OusGEvX2gsklX0t7ceRU9FwU+pAuP8wRTpgt/bLG80oMQpvxY6rp8pPC3lMjXF46wOB8py0QONm3ogL3OVr6n/vPTW+huum6nEaUs2+J5N+HAzygPQoJwKeuq1RCk+QjaB2zd5oLMLHocerd4ydqLQe76OgHBBD7vgL+GHZNBFxgwEGGLCoKuA0t1sCiO9hMgPLbsKIRTT0zCCOmq3X8Pv//c+fqAFQV2zzu09rU/GqYtSIIJVgfJdVSsPAn7W0DyB5RaRa+lVjY3EUKB994HGbjVYAWKiQAFT9eNPsF3Hj32TwKNhqInxTo1cSQ+KVIQ0NHM/zRMYz62i/fmyvyUgN1SGWxqADo3OlR/HXIY2qeTPSwrr/838gMw5eUOMWk/luIpgjp9APJzGuJmY6FoR8egzzNdqAs0u984NUYFGUJ6++thi5+0dfU3aBsg=
