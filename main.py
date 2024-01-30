0714f7629282a2c9a32d53ee4bdf4814e6558791
RizzFa <153635318+RizzFa@users.noreply.github.com>
2024-01-30 20:55:26 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+import http.client
+import json
+import time
+
+with open('./config.json') as f:
+    config_data = json.load(f)
+    channel_id = config_data['Config'][0]['channelid']  # config.json
+    token = config_data['Config'][0]['token']  # config.json
+    message = config_data['Config'][0]['message']  # config.json
+    waktu = config_data['Config'][0]['waktu'] # config.json
+
+header_data = { 
+    "Content-Type": "application/json", 
+    "User-Agent": "DiscordBot", 
+    "Authorization": token  
+} 
+
+def get_connection(): 
+    return http.client.HTTPSConnection("discord.com", 443) 
+
+def send_message(conn, channel_id, message_data): 
+    try: 
+        conn.request("POST", f"/api/v10/channels/{channel_id}/messages", message_data, header_data) 
+        resp = conn.getresponse() 
+         
+        if 199 < resp.status < 300: 
+            print("Pesan sukses terkirim..") 
+        else: 
+            print(f"HTTP {resp.status}: {resp.reason}")
+    except: 
+        print("Error.") 
+
+def main(): 
+    message_data = { 
+        "content": message, 
+        "tts": False
+    } 
+
+    send_message(get_connection(), channel_id, json.dumps(message_data)) 
+
+if __name__ == '__main__': 
+    while True:    
+        main()      
+        time.sleep(waktu)
