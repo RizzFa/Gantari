62b8a8feebc0f2c831074917591fa620874ee685
Gantari <153635318+RizzFa@users.noreply.github.com>
2024-01-31 11:47:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ def Authkey():
     if result[0] == None or not Helpers.IsOnRightMachine(result[0]):
     # an error occurred or the key is invalid or it cannot be activated
     # (eg. the limit of activated devices was achieved)
+        break
         print("The license does not work: {0}".format(result[1]))
-            break
     else:
     # everything went fine if we are here!
         print("The license is valid!")
